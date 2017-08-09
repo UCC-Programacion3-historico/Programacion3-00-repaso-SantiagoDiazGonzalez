1d6565413c145a86ff5320271f1046c8a539c2ba
SantiagoDiazGonzalez <sandiazdiez@gmail.com>
2017-08-09 10:41:12 -0300

    Se implemento el ejercicio II y cambios en el ejercicio I

--- 03_Excepciones/main.cpp
+++ 03_Excepciones/main.cpp
@@ -4,6 +4,18 @@
 using namespace std;
 
 int main() {
+
+    Calculadora<int> calc;
+
+    calc.setA(300);
+    calc.setB(0);
+
+    try{
+        cout << calc.dividir() << endl;
+    }catch(int e){
+        cout << "B no puede ser 0: " << e << endl;
+    }
+
     cout << "Ejercicio 00/03\n" << endl;
     return 0;
 }
